5a8b39d7858845370ff344530f8ae4279a25f416
renandelfino013 <renancontaps@gmail.com>
2026-02-16 15:22:02 -0300

    Update do.js

--- todolist2/do.js
+++ todolist2/do.js
@@ -1,3 +1,4 @@
+
 class Tarefa{ 
   constructor(nometarefa,estado,data){
     this.nometarefa=nometarefa;
@@ -9,19 +10,51 @@ class Tarefa{
 }
 let tarefas = [];
 
-
-function adicionarTarefa(e){
-  e.preventDefault(); // Impede o envio padrão do formulário
-  const newtarefa = new Tarefa (tarefaInput.value)
-  tarefa = tarefa.push(newtarefa)
+const form = document.querySelector("form");
+const lista = document.querySelector("ul");
+const tarefaInput = document.getElementById('tarefa');
 
 
+render()
+function render(){
+
+  const tarefasSalvas = localStorage.getItem("tarefas");
+if (tarefasSalvas) {
+  tarefas = JSON.parse(tarefasSalvas);
+}
+
+  for (const item of tarefas){
+    let li = document.createElement("li")
+    li.textContent = item.nometarefa
+    lista.appendChild(li)
+
+  }
 }
 
 
 
-const form = document.querySelector("form");
-const lista = document.querySelector("ul");
-const tarefaInput = document.getElementById('tarefa');
-let enviar = getElementById("submit")
+function adicionarTarefa(){
+  const newtarefa = tarefas.push (new Tarefa (tarefaInput.value))
+  console.log(tarefas)
+   let lis = document.createElement("li")
+    lis.textContent = tarefaInput.value
+    lista.appendChild(lis)
+
+  form.reset()
+  
+localStorage.setItem("tarefas" , JSON.stringify(tarefas))
+}
+
+
+form.addEventListener('submit' , (e) =>{
+  e.preventDefault(); // Impede o envio padrão do formulário
+
+ adicionarTarefa()
+ 
+
+
+
+
+} )
+
 
