4c1f506a361759b02c886b6a9ce84e053e01a64c
renandelfino013 <renancontaps@gmail.com>
2026-02-16 14:22:23 -0300

    .

---
+++ todolist2/do copy 2.js
@@ -0,0 +1,110 @@
+let tarefa = [];
+
+class tarefas {
+  static proximoid = 1;
+
+  constructor(nometarefa = "", descricao = "", estado = false, criado) {
+    this.nometarefa = nometarefa;
+    this.descrição = descricao;
+    this.id = tarefas.proximoid++;
+    this.estado = estado;
+    this.criado = criado;
+  }
+}
+tarefa = JSON.parse(localStorage.getItem("tarefas"))
+
+function lertare (){
+  if (tarefa === null) {
+
+
+
+}
+else{
+tarefa.forEach(t => {
+     const lis = document.createElement("li");
+    const botao = document.createElement("button")
+    const check = document.createElement("button")
+  lis.textContent = tarefa.nometarefa 
+    botao.textContent = "remove?"
+    check.textContent = "Check?"
+      lista.appendChild(lis)
+      lis.appendChild(botao)
+      lis.appendChild(check)
+    
+
+    
+  });
+}}
+
+function criartarefa() {
+  let data = new Date().toLocaleDateString("pt-br");
+
+  let mome = false;
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    let tarefainput = form.tarefa.value;
+    if (tarefainput === "") return;
+    const lis = document.createElement("li");
+    const botao = document.createElement("button")
+    const check = document.createElement("button")
+
+    botao.textContent = "remove?"
+    check.textContent = "Check?"
+    botao.addEventListener ("click" , (b) =>{
+         const id = Number (lis.dataset.id)
+        tarefa = tarefa.filter( t => t.id !== id)
+        lis.remove()
+localStorage.setItem("tarefas", JSON.stringify(tarefa));
+
+        
+
+
+    }
+  
+  )
+    check.addEventListener("click", (c) => {
+        const id = Number(lis.dataset.id)
+        let tarefaatual = tarefa.find(t =>t.id === id )
+       
+     tarefaatual.estado = !tarefaatual.estado
+     if(tarefaatual.estado === true){
+        check.textContent = "✅"
+        lis.classList.add("concluida")
+    }
+        else{
+            check.textContent = "❌"
+            lis.classList.remove("concluida")
+        }
+        
+        console.log(tarefa)
+localStorage.setItem("tarefas", JSON.stringify(tarefa));
+
+
+    } )
+    
+    lis.textContent = tarefainput
+    
+  
+    lista.appendChild(lis)
+      lis.appendChild(botao)
+      lis.appendChild(check)
+    
+    form.reset()
+    
+    criartaref = new tarefas(tarefainput)
+    tarefa.push(criartaref)
+    lis.dataset.id = criartaref.id
+  
+  
+  
+  
+localStorage.setItem("tarefas", JSON.stringify(tarefa));
+  
+  });
+}
+let form = document.querySelector("form");
+let lista = document.querySelector("ul");
+
+lertare()
+criartarefa()
--- todolist2/do.js
+++ todolist2/do.js
@@ -1,72 +1,27 @@
-let tarefa = [];
+class Tarefa{ 
+  constructor(nometarefa,estado,data){
+    this.nometarefa=nometarefa;
+    this.estado=estado;
+    this.data=data;
 
-class tarefas {
-  static proximoid = 1;
-
-  constructor(nometarefa = "", descricao = "", estado = false, criado) {
-    this.nometarefa = nometarefa;
-    this.descrição = descricao;
-    this.id = tarefas.proximoid++;
-    this.estado = estado;
-    this.criado = criado;
   }
+
+}
+let tarefas = [];
+
+
+function adicionarTarefa(e){
+  e.preventDefault(); // Impede o envio padrão do formulário
+  const newtarefa = new Tarefa (tarefaInput.value)
+  tarefa = tarefa.push(newtarefa)
+
+
 }
 
-function criartarefa() {
-  let data = new Date().toLocaleDateString("pt-br");
-
-  let mome = false;
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    let tarefainput = form.tarefa.value;
-    if (tarefainput === "") return;
-    const lis = document.createElement("li");
-    const botao = document.createElement("button")
-    const check = document.createElement("button")
-
-    botao.textContent = "remove?"
-    check.textContent = "Check?"
-    botao.addEventListener ("click" , (b) =>{
-         const id = Number (lis.dataset.id)
-        tarefa = tarefa.filter( t => t.id !== id)
-        lis.remove()
-        
 
 
-    })
-    check.addEventListener("click", (c) => {
-        const id = Number(lis.dataset.id)
-        let tarefaatual = tarefa.find(t =>t.id === id )
-       
-     tarefaatual.estado = !tarefaatual.estado
-     if(tarefaatual.estado === true){
-        check.textContent = "✅"
-        lis.classList.add("concluida")
-    }
-        else{
-            check.textContent = "❌"
-            lis.classList.remove("concluida")
-        }
-        
-        console.log(tarefa)
+const form = document.querySelector("form");
+const lista = document.querySelector("ul");
+const tarefaInput = document.getElementById('tarefa');
+let enviar = getElementById("submit")
 
-    } )
-    
-    lis.textContent = tarefainput
-    
-  
-    lista.appendChild(lis)
-      lis.appendChild(botao)
-      lis.appendChild(check)
-    
-    form.reset()
-    
-    criartaref = new tarefas(tarefainput)
-    tarefa.push(criartaref)
-    lis.dataset.id = criartaref.id
-  });
-}
-let form = document.querySelector("form");
-let lista = document.querySelector("ul");
-criartarefa()
