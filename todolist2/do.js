6ac5460b760356d41cc11b8ad46798eca9ef4a09
renandelfino013 <renancontaps@gmail.com>
2026-02-17 04:09:23 -0300

    .

--- todolist2/do.js
+++ todolist2/do.js
@@ -1,60 +1,81 @@
-
-class Tarefa{ 
-  constructor(nometarefa,estado,data){
-    this.nometarefa=nometarefa;
-    this.estado=estado;
-    this.data=data;
-
+class Tarefa {
+  constructor(nometarefa, estado, data) {
+    this.nometarefa = nometarefa;
+    this.estado = estado;
+    this.data = data;
   }
-
 }
 let tarefas = [];
 
 const form = document.querySelector("form");
 const lista = document.querySelector("ul");
-const tarefaInput = document.getElementById('tarefa');
-
-
-render()
-function render(){
-
+const tarefaInput = document.getElementById("tarefa");
+const addButton = document.getElementById("add");
+const h3 = document.getElementById("h3");
+const listaTarefas = document.getElementById("listatarefas");
+render();
+function render() {
   const tarefasSalvas = localStorage.getItem("tarefas");
-if (tarefasSalvas) {
-  tarefas = JSON.parse(tarefasSalvas);
+  if (tarefasSalvas) {
+    tarefas = JSON.parse(tarefasSalvas);
+  }
+
+  for (const item of tarefas) {
+    let li = document.createElement("li");
+    li.classList.add("tarefa-item");
+
+    li.textContent = item.nometarefa;
+
+    lista.appendChild(li);
+  }
+  if (tarefasSalvas == false || tarefasSalvas == null) {
+listaTarefas.classList.add("listavazia");
+h3.classList.add("listavazia")  }
 }
 
-  for (const item of tarefas){
-    let li = document.createElement("li")
-    li.textContent = item.nometarefa
-    lista.appendChild(li)
+function adicionarTarefa(item,lis) {
+  if (item.value === "") {
+    alert("Digite uma tarefa válida!");
+    return;
+  } else {
+    const newtarefa = new Tarefa(item.value);
+    tarefas.push(newtarefa);
 
+    console.log(tarefas);
+    lis.textContent = item;
+    lis.classList.add("tarefa-item");
+
+    localStorage.setItem("tarefas", JSON.stringify(tarefas));
   }
 }
+addButton.classList.add("add-button");
+addButton.addEventListener("click", (e) => {
+  let lis = document.createElement("li");
+  let input = document.createElement("input");
+  input.classList.add("inputtarefa");
+  input.type = "text";
+  input.placeholder = "Digite sua tarefa aqui";
+
+  lis.classList.add("tarefa-item");
+   lista.appendChild(lis);
+      lis.appendChild(input);
+      input.focus();
 
 
+      
 
-function adicionarTarefa(){
-  const newtarefa = tarefas.push (new Tarefa (tarefaInput.value))
-  console.log(tarefas)
-   let lis = document.createElement("li")
-    lis.textContent = tarefaInput.value
-    lista.appendChild(lis)
+  input.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+      if (input.value.trim() === "") {
+        alert("Digite uma tarefa válida!");
+      } else {
+        
 
-  form.reset()
-  
-localStorage.setItem("tarefas" , JSON.stringify(tarefas))
-}
+        adicionarTarefa(input,lis);
+     
+      }
 
-
-form.addEventListener('submit' , (e) =>{
-  e.preventDefault(); // Impede o envio padrão do formulário
-
- adicionarTarefa()
- 
-
-
-
-
-} )
-
-
+      
+    }
+  });
+});
