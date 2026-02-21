96e3bf1eb070ba51f5c8acc646f6b9ac1925c6eb
renandelfino013 <renancontaps@gmail.com>
2026-02-21 15:04:54 -0300

    ajustes

--- loja/js/products.js
+++ loja/js/products.js
@@ -5,12 +5,14 @@ let produtos = [
       ' Monitor Gamer LG UltraGear 27", FHD, 180Hz, 1ms, IPS',
     preço: 1299.99,
     img: "./assets/monitorpreto.png",
+    quantidade: 1,
   },
   {
     id:2,
     nomeproduto:' SSD Kingston NV3, 2TB, M.2 2280',
     preço:1999.99,
     img: "./assets/ssdpreto.png",
+    quantidade: 1,
 
   },
   {
@@ -18,12 +20,16 @@ let produtos = [
     nomeproduto:'Placa de Vídeo XFX AMD RADEON RX 7600 Gaming Graphics Card, 8GB,', 
     preço: 1899.99,
     img: "./assets/placapreta.png",
+    quantidade: 1,
+
   },
   {
     id: 4,
   nomeproduto:'Console PlayStation 5 Pro Sony, SSD 2TB,',  
   preço: 4999.99,
-  img: "./assets/ps5preto.png"
+  img: "./assets/ps5preto.png",
+    quantidade: 1,
+
   }
 
 ];
