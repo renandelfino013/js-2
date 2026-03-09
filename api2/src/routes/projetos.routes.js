413e60da488ff2975876bcf7379b98e6620243f6
renandelfino013 <renancontaps@gmail.com>
2026-03-09 15:49:34 -0300

    .

--- api2/src/routes/projetos.routes.js
+++ api2/src/routes/projetos.routes.js
@@ -1,6 +1,7 @@
 const express =require("express") ;
 const router = express.Router()
 const projetosController = require("../controllers/projetos.controller")
+const tarefascontroller= require("../controllers/tarefas.controller")
 
 
 
