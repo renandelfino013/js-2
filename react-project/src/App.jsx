f95f70da29d4c034386a5eec6a8173a8f2f059a9
renandelfino013 <renancontaps@gmail.com>
2026-03-19 00:44:13 -0300

    .

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,12 +1,18 @@
 import { useEffect, useState } from "react";
+
 function App() {
   let [mostrarinput, setmostrarinput] = useState(false);
   let [mostrarprojetos, setmostrarprojetos] = useState(false);
   let [texto, settexto] = useState("");
+  let [nometarefa, setnometarefa] = useState("");
+  let [adionandotarefa, setadioconandotarefa] = useState(null);
   let [projetos, setprojetos] = useState([]);
   let [tarefas, settarefas] = useState([]);
   let [projetoSelecionado, setProjetoSelecionado] = useState(null);
   let [clicado, setclicado] = useState("");
+  let [textoedicao, settextoedicao] = useState("");
+  let [editandoid, seteditandoid] = useState(null);
+
   useEffect(() => {
     chamarapi();
   }, []);
@@ -16,14 +22,17 @@ function App() {
     let dados = await resp.json();
     setprojetos(dados);
   }
+
   function mudartexto(e) {
     settexto(e.target.value);
   }
+
   async function tarefasprojeto(id) {
     let resp = await fetch(`/projetos/${id}/listartarefas`);
     let tarefas = await resp.json();
     settarefas(tarefas);
     setProjetoSelecionado(id);
+
     return tarefas.map((tarefa) => {
       return (
         <li key={tarefa.id}>
@@ -32,78 +41,205 @@ function App() {
       );
     });
   }
+
   function verificarestado(estado) {
     if (estado === true) {
       estado = "ativo";
       setverifyestado(estado);
     }
   }
+
+  async function mudarnome(id) {
+    let resp = await fetch(`/projetos/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        nome: `${textoedicao}`,
+      }),
+    });
+
+    if (resp.ok) {
+      await chamarapi();
+      seteditandoid(null);
+      settextoedicao("");
+    }
+  }
+
+  async function adiocionartarefa(id, nome) {
+    let resp = await fetch(`/projetos/${id}/tarefas`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        nome: `${nome}`,
+      }),
+    });
+    if(resp.ok){
+      setadioconandotarefa(null)
+      await tarefasprojeto(id)
+      await chamarapi()
+    }
+  }
+
+  async function deleteprojeto(id) {
+    let resp = await fetch(`/projetos/${id}`, {
+      method: "DELETE",
+    });
+
+    if (resp.ok) {
+      await chamarapi();
+    }
+  }
+
   function Mylista() {
     return (
       <ul className="listas">
         {projetos.map((projeto) => (
-          <li key={projeto.id}>
+          <li key={projeto.id} className="lip">
             <div onClick={() => tarefasprojeto(projeto.id)} className="projeto">
               <div className="divtitulo">
-                <h3 className="tituloprojeto"> {projeto.nome}</h3>
+                {editandoid === projeto.id ? (
+                  <input
+                    className="inputtitulo"
+                    type="text"
+                    value={textoedicao}
+                    autoFocus
+                    onChange={(e) => settextoedicao(e.target.value)}
+                    onClick={(e) => e.stopPropagation()}
+                    onBlur={() => mudarnome(projeto.id)}
+                    onKeyDown={(e) => {
+                      if (e.key === "Enter") {
+                        mudarnome(projeto.id);
+                      }
+                      if (e.key === "Escape") {
+                        seteditandoid(null);
+                        settextoedicao("");
+                      }
+                    }}
+                  />
+                ) : (
+                  <h3
+                    className="tituloprojeto"
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      seteditandoid(projeto.id);
+                      settextoedicao(projeto.nome);
+                    }}
+                  >
+                    {projeto.nome}
+                  </h3>
+                )}
+
                 <h3 className="estadoprojeto">
                   {projeto.estado ? "ativo" : "inativo"}
                 </h3>
               </div>
-              <p>{}</p>
+
+            
+
               {projetoSelecionado === projeto.id && (
                 <ul id="listatarefas">
                   {tarefas.map((tarefa) => (
                     <li key={tarefa.id} className="litarefa">
                       <div className="divtitulo">
-                        <p>
-                          {tarefa.nome} - {String(tarefa.estado)}{" "}
-                        </p>
+                      <p className="txttarefa">
+              <span className="nometarefa">{tarefa.nome}</span>
+                <span className="statustarefa">a fazer</span>
+</p>
 
-                        <p className="data">
-                          {new Date(tarefa.criadoem)
-                            .toLocaleString("pt-BR", {
-                              month: "2-digit",
-                              year: "numeric",
-                              day: "2-digit",
-                            })
-                            .replaceAll("/", "-")}
-                        </p>
+                        <div className="direitatarefa">
+                          <p className="data">
+                            {new Date(tarefa.criadoem)
+                              .toLocaleString("pt-BR", {
+                                month: "2-digit",
+                                year: "numeric",
+                                day: "2-digit",
+                              })
+                              .replaceAll("/", "-")}
+                          </p>
+
+                          <button className="buttondeletetarefa"></button>
+</div>
                       </div>
+                
                     </li>
                   ))}
                 </ul>
               )}
+                {adionandotarefa === projeto.id ? (
+                <input
+                className="inputtarefa"
+                  type="text"
+                  autoFocus
+                  value={nometarefa}
+                  onChange={(e) => setnometarefa(e.target.value)}
+                  onClick={(e) => e.stopPropagation()}
+                  onBlur={() => adiocionartarefa(projeto.id, nometarefa)}
+                   onKeyDown={async (e)   =>  {
+                      if (e.key === "Enter") {
+                        adiocionartarefa(projeto.id, nometarefa);
+                        await chamarapi()
+                      }
+                      if (e.key === "Escape") {
+                        setadioconandotarefa(null);
+                        setnometarefa("");
+                      }
+                    }}
+                />
+                
+              ) : (
+                <p
+                  className="pp"
+                  onClick={() => {
+                    setadioconandotarefa(projeto.id);
+                  }}
+                >
+                  +
+                </p>
+              )}
             </div>
+
+            <button
+              className="buttondelete"
+              onClick={() => {
+                deleteprojeto(projeto.id);
+              }}
+            ></button>
           </li>
         ))}
       </ul>
     );
   }
+
   function mostrar() {
     setmostrarprojetos(!mostrarprojetos);
   }
+
   function inputtext() {
     setmostrarinput(!mostrarinput);
   }
+
   async function addprojeto() {
     const resp = await fetch("/projetos", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      nome: `${texto}`
-    })
-  });
-    
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        nome: `${texto}`,
+      }),
+    });
+
     let dados = await resp.json();
-    setprojetos([...projetos,dados]);
+    setprojetos([...projetos, dados]);
     return chamarapi();
   }
+
   function Mybutton(props) {
     return (
-
       <button
         className={clicado ? "buttonb" : "botaodesativado"}
         onClick={() => {
@@ -111,16 +247,17 @@ function App() {
           inputtext();
         }}
       >
-        {" "}
-        {props.children}{" "}
+        {props.children}
       </button>
     );
   }
+
   return (
     <div id="main">
       <div id="conteudo">
         <div className="paibt">
           <Mybutton>+</Mybutton>
+
           {mostrarinput && (
             <input
               className="input"
@@ -136,7 +273,7 @@ function App() {
           )}
         </div>
 
-        <Mylista></Mylista>
+        <Mylista />
       </div>
     </div>
   );
