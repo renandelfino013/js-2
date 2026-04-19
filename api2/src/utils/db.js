2384ad2386c4ee69f9536252aad8aee8aff0134d
Renan Delfino <164447764+renandelfino013@users.noreply.github.com>
2026-04-19 17:30:31 -0300

    Update db.js to use environment variables for database connection

--- api2/src/utils/db.js
+++ api2/src/utils/db.js
@@ -4,14 +4,13 @@ const arquivo = path.join(__dirname ,"..", "data" , "db.json")
 
 const {Client} = require("pg")
 const con = new Client({
-  user: "postgres",
-  host: "localhost",
-  database: "to_do",
-  password: "321776renan",
-  port: 5432,
+  user: process.env.DB_USER || "postgres",
+  host: process.env.DB_HOST || "localhost",
+  database: process.env.DB_NAME || "to_do",
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT || 5432,
 })
 con.connect().then(() => console.log("conectado ao banco de dados"))
-
 
 
 module.exports = con
